82a2f31738e5955407abb61ffae0b2d953378e62
Roman <romprogramist@gmail.com>
2021-01-13 00:54:04 +0300

    ss

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// var hotel = {
+//     name: 'Пляж',
+//     rooms: 120,
+//     booked: 77,
+//     checkAvailability: function() {
+//         return this.rooms - this.booked
+//     }
+// };
+
+// var elName = document.getElementById('hotelName');
+// elName.textContent = hotel.name;
+
+
+// var elRooms = document.getElementById('rooms');
+// elRooms.textContent = hotel.checkAvailability();
+// console.log('hellow');
+
+
+console.log('322')
